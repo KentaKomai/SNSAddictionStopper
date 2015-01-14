f880f7c907d441281412698043632c60a07190ea
Kenta Komai <kaede06152000@gmail.com>
2015-01-14 23:39:11 +0900

    とりあえずオンオフまで 参考　http://www.ibm.com/developerworks/jp/opensource/library/os-extendchrome/

--- js/popup.js
+++ js/popup.js
@@ -2,5 +2,17 @@ $(function () {
     chrome.tabs.getSelected(null, function(tab) {
         $('#title').text(tab.title);
         $('#url').text(tab.url);
+
+        $('#facebookHiddenSwitch').change( function(){
+            if( $(this).is(':checked') ){ $('#facebookNotification').text("Hidden"); }
+            else { $('#facebookNotification').text(""); }
+        });
+        $('#twitterHiddenSwitch').change( function(){
+            if( $(this).is(':checked') ){ $('#twitterkNotification').text("Hidden"); }
+            else {$('#twitterkNotification').text(""); }
+        });
+
     });
+
+    
 });
